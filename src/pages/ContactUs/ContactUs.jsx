8a4b819976b7fd04e98a6c1921a9f7f627281860
Tony <tonymike99@gmail.com>
2023-01-04 03:25:04 +0530

    modified ContactUs page styles

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -1,7 +1,11 @@
+import styles from "./ContactUs.module.css";
+
 function ContactUs() {
   return (
-    <main className="p-6 flex flex-col items-center">
-      <div className="w-3/4 xl:w-1/2">
+    <main
+      className={styles["main-container"] + " p-6 flex flex-col items-center"}
+    >
+      <div className="w-3/4 lg:w-1/2 xl:w-1/4">
         <h1 className="text-4xl font-bold mb-6 text-center">Contact Us</h1>
         <form className="flex flex-col gap-6" action="" method="POST">
           <input
@@ -10,6 +14,7 @@ function ContactUs() {
             id="name"
             placeholder="Name"
             required
+            className="border"
           />
           <input
             type="email"
@@ -17,6 +22,7 @@ function ContactUs() {
             id="email"
             placeholder="Email"
             required
+            className="border"
           />
           <textarea
             name="description"
@@ -25,7 +31,14 @@ function ContactUs() {
             rows="10"
             placeholder="Description"
             required
+            className="border"
           ></textarea>
+          <button
+            type="submit"
+            className="bg-blue-500 text-white py-4 px-2 rounded-lg"
+          >
+            Submit
+          </button>
         </form>
       </div>
     </main>
