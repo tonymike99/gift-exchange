abee3f7be402b273f2fce38c9cdc688cb8606106
Tony <tonymike99@gmail.com>
2023-01-03 20:06:57 +0530

    added component to display the header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header className="flex flex-row justify-between items-center p-6">
+      <h1 className="cursive text-5xl">
+        <Link to="/">Gift Exchange</Link>
+      </h1>
+      <div className="flex flex-row gap-4">
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contact Us</Link>
+        <Link to="/login">Login/Signup</Link>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
