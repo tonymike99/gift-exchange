0252fdd5e81e0d6bb1ccc5d6c0af8f980e20b6af
Tony <tonymike99@gmail.com>
2023-01-04 01:31:18 +0530

    made the Header component responsive to all screens

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,41 @@
 import { Link } from "react-router-dom";
 import { DarkMode } from "../index";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useState } from "react";
 
 function Header() {
+  const [expandMenu, setExpandMenu] = useState(false);
+
+  const handleExpandMenuOnClick = () => {
+    setExpandMenu(!expandMenu);
+  };
+
   return (
-    <header className="flex flex-row justify-between items-center p-6">
-      <h1 className="cursive text-5xl">
-        <Link to="/">Gift Exchange</Link>
-      </h1>
-      <div className="flex flex-row gap-4">
+    <header className="p-6">
+      <div className="flex flex-row justify-between items-center">
+        <h1 className="cursive text-5xl">
+          <Link to="/">Gift Exchange</Link>
+        </h1>
+        <div className="hidden md:flex flex-row gap-4">
+          <Link to="/">Home</Link>
+          <Link to="/contact">Contact Us</Link>
+          <Link to="/login">Login/Signup</Link>
+          <DarkMode />
+        </div>
+        <span
+          className="cursor-pointer md:hidden"
+          onClick={handleExpandMenuOnClick}
+        >
+          <MenuIcon />
+        </span>
+      </div>
+      <div
+        className={
+          expandMenu
+            ? "md:hidden flex flex-row gap-4 justify-center pt-6"
+            : "hidden"
+        }
+      >
         <Link to="/">Home</Link>
         <Link to="/contact">Contact Us</Link>
         <Link to="/login">Login/Signup</Link>
