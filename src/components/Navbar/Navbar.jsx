7f87c819b591dfe66ecafaa6db398afd3f14c0d0
Tony <tonymike99@gmail.com>
2023-01-04 04:20:53 +0530

    changed Link path of lists

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,13 +6,13 @@ import FolderOpenIcon from "@mui/icons-material/FolderOpen";
 function Navbar() {
   return (
     <nav className="fixed bottom-0 w-full flex flex-row justify-between py-6">
-      <Link to="/personal" className="ml-3 px-3">
+      <Link to="/lists/personal" className="ml-3 px-3">
         <FolderSpecialIcon /> Personal
       </Link>
-      <Link to="/shared-with-me" className="ml-3 px-3">
+      <Link to="/lists/shared-with-me" className="ml-3 px-3">
         <FolderSharedIcon /> Shared with Me
       </Link>
-      <Link to="/public" className="ml-3 px-3">
+      <Link to="/lists/public" className="ml-3 px-3">
         <FolderOpenIcon /> Public
       </Link>
     </nav>
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -8,13 +8,13 @@ function Sidebar() {
     <aside className="p-6 w-full grid">
       <div className="flex flex-col gap-1">
         <span className="text-xl">Lists</span>
-        <Link to="/personal" className="ml-3 px-3 py-1">
+        <Link to="/lists/personal" className="ml-3 px-3 py-1">
           <FolderSpecialIcon /> Personal
         </Link>
-        <Link to="/shared-with-me" className="ml-3 px-3 py-1">
+        <Link to="/lists/shared-with-me" className="ml-3 px-3 py-1">
           <FolderSharedIcon /> Shared with Me
         </Link>
-        <Link to="/public" className="ml-3 px-3 py-1">
+        <Link to="/lists/public" className="ml-3 px-3 py-1">
           <FolderOpenIcon /> Public
         </Link>
       </div>
