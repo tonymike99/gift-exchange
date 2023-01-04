6affbef4c3b7ffcebbc46bd3c73488de89795225
Tony <tonymike99@gmail.com>
2023-01-04 16:10:15 +0530

    added ListTable component to display the lists

---
+++ src/components/ListTable/ListTable.jsx
@@ -0,0 +1,43 @@
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import { Link } from "react-router-dom";
+
+function ListTable() {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>No.</th>
+          <th>Name</th>
+          <th>Visibility</th>
+          <th>Options</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1.</td>
+          <td>
+            <Link to={`/`} className="underline">
+              Birthday
+            </Link>
+          </td>
+          <td>Private</td>
+          <td>
+            <span className="cursor-pointer">
+              <EditIcon color="info" />
+            </span>
+            <span className="cursor-pointer">
+              <DeleteIcon color="error" />
+            </span>
+            <span className="cursor-pointer">
+              <ExitToAppIcon color="error" />
+            </span>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
+
+export default ListTable;
--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Footer from "./Footer/Footer";
 import DarkMode from "./DarkMode/DarkMode";
 import Sidebar from "./Sidebar/Sidebar";
 import Navbar from "./Navbar/Navbar";
+import ListTable from "./ListTable/ListTable";
 
-export { Header, Footer, DarkMode, Sidebar, Navbar };
+export { Header, Footer, DarkMode, Sidebar, Navbar, ListTable };
