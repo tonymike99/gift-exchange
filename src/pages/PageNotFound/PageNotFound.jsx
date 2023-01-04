e53979d6cef47dbf8abec1a44e8e02bdb9810b74
Tony <tonymike99@gmail.com>
2023-01-04 15:49:54 +0530

    modified 404 page styles

--- src/pages/PageNotFound/PageNotFound.jsx
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -2,8 +2,8 @@ const errorImage = require("../../static/img/404_error.svg");
 
 function PageNotFound() {
   return (
-    <main>
-      <img className="" src={errorImage.default} alt="" />
+    <main className="p-6 md:w-4/5 lg:w-3/5 xl:w-2/5 mx-auto">
+      <img src={errorImage.default} alt="" />
     </main>
   );
 }
