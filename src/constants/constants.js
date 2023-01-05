8505fc0405791fffde9e198a4b6c0d23f8e2dc66
Tony <tonymike99@gmail.com>
2023-01-05 18:21:08 +0530

    added constants for the app

---
+++ src/constants/constants.js
@@ -0,0 +1,8 @@
+const constants = {
+  APP_NAME_UPPER_CASE: "GIFT EXCHANGE",
+  APP_NAME_SENTENCE_CASE: "Gift Exchange",
+  APP_NAME_KEBAB_CASE: "gift-exchange",
+  SERVER_URL: "http://localhost:3001",
+};
+
+module.exports = constants;
