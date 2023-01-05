3ba4b12736d4f51c54b7a3fcd48fa3a30de61e9c
Tony <tonymike99@gmail.com>
2023-01-06 02:38:18 +0530

    modified MenuList styles

--- src/components/MenuList/MenuList.jsx
+++ src/components/MenuList/MenuList.jsx
@@ -1,22 +1,24 @@
 import { SERVER_URL } from "../../constants/constants";
 import { Link } from "react-router-dom";
 
-function MenuList() {
+function MenuList({ setExpandMenu }) {
   return (
-    <div className="rounded">
+    <>
       <Link
         to="/profile"
-        className="hover:bg-slate-500 cursor-pointer py-1 pl-2 pr-10 rounded block"
+        className="hover:bg-slate-500 cursor-pointer py-1 pl-2 pr-4 rounded block"
+        onClick={() => setExpandMenu(false)}
       >
         Profile
       </Link>
       <a
         href={SERVER_URL + "/logout"}
-        className="hover:bg-slate-500 cursor-pointer py-1 pl-2 pr-10 rounded block"
+        className="hover:bg-slate-500 cursor-pointer py-1 pl-2 pr-4 rounded block"
+        onClick={() => setExpandMenu(false)}
       >
         Logout
       </a>
-    </div>
+    </>
   );
 }
 
