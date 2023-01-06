0c3194a46fb44069e897e6fefaadb20da3578deb
Tony <tonymike99@gmail.com>
2023-01-06 14:33:28 +0530

    converted SERVER_URL into env variable

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,8 +1,11 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 const constants = {
   APP_NAME_UPPER_CASE: "GIFT EXCHANGE",
   APP_NAME_SENTENCE_CASE: "Gift Exchange",
   APP_NAME_KEBAB_CASE: "gift-exchange",
-  SERVER_URL: "http://localhost:3001",
+  SERVER_URL: process.env.SERVER_URL,
 };
 
 module.exports = constants;
