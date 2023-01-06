0bf15db0cff8c02328c56fffe6fc1ee80d340ba7
Tony <tonymike99@gmail.com>
2023-01-06 07:29:12 +0530

    removed locally stored user id on logout

--- src/components/MenuList/MenuList.jsx
+++ src/components/MenuList/MenuList.jsx
@@ -2,19 +2,27 @@ import { SERVER_URL } from "../../constants/constants";
 import { Link } from "react-router-dom";
 
 function MenuList({ setExpandMenu }) {
+  const handleProfileOnClick = () => {
+    setExpandMenu(false);
+  };
+
+  const handleLogoutOnClick = () => {
+    localStorage.removeItem("userId");
+  };
+
   return (
     <>
       <Link
         to="/profile"
         className="hover:bg-slate-500 cursor-pointer py-1 pl-2 pr-4 rounded block"
-        onClick={() => setExpandMenu(false)}
+        onClick={handleProfileOnClick}
       >
         Profile
       </Link>
       <a
         href={SERVER_URL + "/logout"}
         className="hover:bg-slate-500 cursor-pointer py-1 pl-2 pr-4 rounded block"
-        onClick={() => setExpandMenu(false)}
+        onClick={handleLogoutOnClick}
       >
         Logout
       </a>
