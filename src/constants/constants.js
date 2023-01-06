ffef4e3a4e9d2a1d2f8ff4c3d10308897dbbaa3b
Tony <tonymike99@gmail.com>
2023-01-06 14:55:01 +0530

    converted SERVER_URL constant into an env variable

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,7 +2,7 @@ const constants = {
   APP_NAME_UPPER_CASE: "GIFT EXCHANGE",
   APP_NAME_SENTENCE_CASE: "Gift Exchange",
   APP_NAME_KEBAB_CASE: "gift-exchange",
-  SERVER_URL: "http://localhost:3001",
+  SERVER_URL: process.env.REACT_APP_SERVER_URL,
 };
 
 module.exports = constants;
