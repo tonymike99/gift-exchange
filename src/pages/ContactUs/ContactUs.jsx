9c9303e31d19bccdbeaed1fb4b23980776d6901f
Tony <tonymike99@gmail.com>
2023-01-06 08:15:13 +0530

    updated action url for contact form and added maxlength fields for input/textarea

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -1,45 +1,62 @@
 import styles from "./ContactUs.module.css";
+import { SERVER_URL } from "../../constants/constants";
+import { toast } from "react-toastify";
 
 function ContactUs() {
+  const storedTheme = localStorage.getItem("theme");
+  const toastTheme = storedTheme === "light" ? "dark" : "light";
+
   return (
     <main
       className={styles["main-container"] + " p-6 flex flex-col items-center"}
     >
       <div className="w-3/4 lg:w-1/2 xl:w-1/4">
         <h1 className="text-4xl font-bold mb-6 text-center">Contact Us</h1>
-        <form className="flex flex-col gap-6" action="" method="POST">
+        <form
+          className="flex flex-col gap-6"
+          action={SERVER_URL + "/contactus"}
+          method="POST"
+          onSubmit={toast.success("Your response has been recorded!", {
+            theme: toastTheme,
+            toastId: "1",
+          })}
+        >
           <input
+            className="border"
             type="text"
             name="name"
             id="name"
             placeholder="Name"
             required
-            className="border"
+            maxlength="100"
           />
           <input
+            className="border"
             type="email"
             name="email"
             id="email"
             placeholder="Email"
             required
-            className="border"
+            maxlength="100"
           />
           <input
+            className="border"
             type="text"
             name="subject"
             id="subject"
             placeholder="Subject"
             required
-            className="border"
+            maxlength="250"
           />
           <textarea
+            className="border"
             name="description"
             id="description"
             cols="30"
             rows="8"
             placeholder="Description"
             required
-            className="border"
+            maxlength="750"
           ></textarea>
           <button
             type="submit"
