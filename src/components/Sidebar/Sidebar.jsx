aefb6acc34c477ef8325bf17afaf03589bb2eadc
Tony <tonymike99@gmail.com>
2023-01-07 21:42:08 +0530

    updated sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,21 +1,41 @@
 import { Link } from "react-router-dom";
-import FolderSpecialIcon from "@mui/icons-material/FolderSpecial";
+import AddIcon from "@mui/icons-material/Add";
+import EditIcon from "@mui/icons-material/Edit";
 import FolderSharedIcon from "@mui/icons-material/FolderShared";
-import FolderOpenIcon from "@mui/icons-material/FolderOpen";
+import TravelExploreIcon from "@mui/icons-material/TravelExplore";
+import MarkEmailUnreadIcon from "@mui/icons-material/MarkEmailUnread";
 
 function Sidebar() {
+  const handleCreateListButtonOnClick = (e) => {
+    e.target.classList.add("opacity-50");
+    e.target.disabled = true;
+    setTimeout(() => {
+      e.target.classList.remove("opacity-50");
+      e.target.disabled = false;
+    }, 2000);
+  };
+
   return (
     <aside className="p-6 w-full grid">
-      <div className="flex flex-col gap-1">
-        <span className="text-xl">Lists</span>
-        <Link to="/lists/personal" className="ml-3 px-3 py-1">
-          <FolderSpecialIcon /> Personal
+      <div className="flex flex-col gap-2">
+        <button
+          className="bg-blue-500 text-white -ml-1 px-2 py-1 rounded-lg w-32"
+          onClick={handleCreateListButtonOnClick}
+        >
+          <AddIcon /> Create list
+        </button>
+
+        <Link to="/lists/created" className="px-3 py-1">
+          <EditIcon /> Created Lists
         </Link>
-        <Link to="/lists/shared-with-me" className="ml-3 px-3 py-1">
-          <FolderSharedIcon /> Shared with Me
+        <Link to="/lists/joined" className="px-3 py-1">
+          <FolderSharedIcon /> Joined Lists
         </Link>
-        <Link to="/lists/public" className="ml-3 px-3 py-1">
-          <FolderOpenIcon /> Public
+        <Link to="/lists/discover" className="px-3 py-1">
+          <TravelExploreIcon /> Discover Lists
+        </Link>
+        <Link to="/lists/invitations" className="px-3 py-1">
+          <MarkEmailUnreadIcon /> Invitations
         </Link>
       </div>
     </aside>
