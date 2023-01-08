1b3a90791b6af5cad641b10954b78471a5799ff2
Tony <tonymike99@gmail.com>
2023-01-09 03:30:24 +0530

    displayed buttons on the lists table according to the url path

--- src/components/ListsTable/ListsTable.jsx
+++ src/components/ListsTable/ListsTable.jsx
@@ -2,9 +2,11 @@ import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import CheckIcon from "@mui/icons-material/Check";
 import ClearIcon from "@mui/icons-material/Clear";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
-function ListsTable() {
+function ListsTable({ lists }) {
+  const location = useLocation();
+
   return (
     <table>
       <thead>
@@ -16,29 +18,62 @@ function ListsTable() {
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>1.</td>
-          <td>
-            <Link to={`/`} className="underline">
-              Birthday
-            </Link>
-          </td>
-          <td>Private</td>
-          <td>
-            <span className="cursor-pointer">
-              <EditIcon color="info" />
-            </span>
-            <span className="cursor-pointer">
-              <DeleteIcon color="error" />
-            </span>
-            <span className="cursor-pointer">
-              <CheckIcon color="info" />
-            </span>
-            <span className="cursor-pointer">
-              <ClearIcon color="error" />
-            </span>
-          </td>
-        </tr>
+        {lists.map((list, index) => (
+          <tr key={list._id}>
+            <td>{index + 1}.</td>
+            <td>
+              <Link
+                to={`${location.pathname}/${list._id}`}
+                className="underline capitalize"
+              >
+                {list.name}
+              </Link>
+            </td>
+            <td className="capitalize">{list.visibility}</td>
+            <td>
+              {location.pathname === "/lists/created" && (
+                <>
+                  <span className="cursor-pointer">
+                    <EditIcon color="info" />
+                  </span>
+                  <span className="cursor-pointer">
+                    <DeleteIcon color="error" />
+                  </span>
+                </>
+              )}
+
+              {location.pathname === "/lists/joined" && (
+                <>
+                  <span className="cursor-pointer">
+                    <ClearIcon color="error" />
+                  </span>
+                </>
+              )}
+
+              {location.pathname === "/lists/discover" && (
+                <>
+                  <span className="cursor-pointer">
+                    <CheckIcon color="info" />
+                  </span>
+                  <span className="cursor-pointer">
+                    <ClearIcon color="error" />
+                  </span>
+                </>
+              )}
+
+              {location.pathname === "/lists/invitations" && (
+                <>
+                  <span className="cursor-pointer">
+                    <CheckIcon color="info" />
+                  </span>
+                  <span className="cursor-pointer">
+                    <ClearIcon color="error" />
+                  </span>
+                </>
+              )}
+            </td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
