6599f0cccb3e1627cebbe2809085eb626a932206
Tony <tonymike99@gmail.com>
2023-01-16 19:18:31 +0530

    wrapped table under a div to make it responsive

--- src/components/WishlistProductsTable/WishlistProductsTable.jsx
+++ src/components/WishlistProductsTable/WishlistProductsTable.jsx
@@ -1,56 +1,61 @@
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
-import TaskAltIcon from "@mui/icons-material/TaskAlt";
+import DoneAllIcon from "@mui/icons-material/DoneAll";
 import RemoveDoneIcon from "@mui/icons-material/RemoveDone";
-import { Link } from "react-router-dom";
 
-function WishlistProductsTable({ wishlistProducts }) {
+function WishlistProductsTable({ wishlist }) {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>No.</th>
-          <th>Id</th>
-          <th>Picture</th>
-          <th>Name</th>
-          <th>Price</th>
-          <th>Link</th>
-          <th>Notes</th>
-          <th>Options</th>
-        </tr>
-      </thead>
-      <tbody>
-        {wishlistProducts.map((product, index) => (
+    <div className="table-wrapper">
+      <table>
+        <thead>
           <tr>
-            <td>{index}</td>
-            <td>{product.id}</td>
-            <td>{product.picture}</td>
-            <td>{product.name}</td>
-            <td>{product.price}</td>
-            <td>
-              <Link to={product.link} className="underline">
-                {product.link}
-              </Link>
-            </td>
-            <td>{product.notes}</td>
-            <td>
-              <span className="cursor-pointer">
-                <EditIcon color="info" />
-              </span>
-              <span className="cursor-pointer">
-                <DeleteIcon color="error" />
-              </span>
-              <span className="cursor-pointer">
-                <TaskAltIcon color="info" />
-              </span>
-              <span className="cursor-pointer">
-                <RemoveDoneIcon color="error" />
-              </span>
-            </td>
+            <th>No.</th>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Link</th>
+            <th>Notes</th>
+            <th>Options</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {wishlist?.map((product, index) => (
+            <tr
+              key={product._id}
+              className={product.isDone ? "line-through" : ""}
+            >
+              <td>{index}</td>
+              <td>{product.picture}</td>
+              <td>{product.name}</td>
+              <td>{product.price}</td>
+              <td>
+                <a href={product.link} className="underline">
+                  {product.link}
+                </a>
+              </td>
+              <td>{product.notes}</td>
+              <td>
+                <span className="cursor-pointer">
+                  <EditIcon color="info" />
+                </span>
+                <span className="cursor-pointer">
+                  <DeleteIcon color="error" />
+                </span>
+                {product.isDone ? (
+                  <span className="cursor-pointer">
+                    <RemoveDoneIcon color="error" />
+                  </span>
+                ) : (
+                  <span className="cursor-pointer">
+                    <DoneAllIcon color="info" />
+                  </span>
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
